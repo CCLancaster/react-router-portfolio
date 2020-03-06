3b664bb16042087668a0352b15f985e36d71e160
CCLancaster <connieclancaster@gmail.com>
2020-03-05 18:10:29 -0800

    filled in blog posts to populate my blog page, added links to About

--- src/About.js
+++ src/About.js
@@ -4,6 +4,8 @@ function About(props) {
     return (
         <div>
             <h1>About</h1>
+            <a href="https://www.linkedin.com/in/cclancaster/">LinkedIn</a> |
+            | <a href="https://github.com/CCLancaster">GitHub</a> 
         </div>
     )
 }
